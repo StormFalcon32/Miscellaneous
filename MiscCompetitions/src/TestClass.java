fe8636b6948218a652dea23092cbc90c1c6b025c
StormFalcon32 <benchen9708@gmail.com>
2018-12-30 12:38:32 -0500

    Attempted Benny and Subsets

--- MiscCompetitions/src/TestClass.java
+++ MiscCompetitions/src/TestClass.java
@@ -1,12 +1,15 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class TestClass {
+	
 	public static void main(String args[]) throws IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		int t = Integer.parseInt(in.readLine());
-		for (int i = 0; i < t; i++) {
+		StringTokenizer ln = new StringTokenizer(in.readLine());
+		int n = Integer.parseInt(ln.nextToken());
+		for (int i = 0; i < n; i++) {
 			
 		}
 	}
