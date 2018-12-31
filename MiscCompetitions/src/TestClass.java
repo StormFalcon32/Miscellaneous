a4937498a0407ba33ec43c1e76fd5416caebd202
StormFalcon32 <benchen9708@gmail.com>
2018-12-30 22:07:17 -0500

    Fixed

--- MiscCompetitions/src/TestClass.java
+++ MiscCompetitions/src/TestClass.java
@@ -4,7 +4,7 @@ import java.io.InputStreamReader;
 
 public class TestClass {
 	
-	static int[] dp;
+	static long[] dp;
 	
 	public static void main(String args[]) throws IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
@@ -15,8 +15,8 @@ public class TestClass {
            inputs[i] = Integer.parseInt(in.readLine());   
            max = Math.max(max, inputs[i]);
         }
-        dp = new int[max + 1];
-        dp[0] = 0;
+        dp = new long[max + 1];
+        dp[0] = 1;
         dp[1] = 1;
         for (int i = 2; i <= max; i++) {
             dp[i] = (dp[i - 1] * i) % (1000000000 + 7);
